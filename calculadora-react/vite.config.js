624703978885ae8581486851a82b6119dabcc1f1
Joel Nerio <joelnerio42@gmail.com>
2026-05-26 20:21:41 -0600

    configurar base path dinámico para dev y produccion es decir para el servidor de clase

--- calculadora-react/vite.config.js
+++ calculadora-react/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/24253/calculadora/' : '/',
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.js',
     globals: true
   }
-})
+}))
